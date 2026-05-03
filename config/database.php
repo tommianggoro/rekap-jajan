f91753b1e690aafdfeb70795da6ca8e518b9a82a
Tommy Anggoro Yunar <tommianggoro@outlook.com>
2026-05-03 12:35:48 +0700

    handle error pdo

--- config/database.php
+++ config/database.php
@@ -11,7 +11,7 @@ try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4", $user, $pass, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
+        // PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
     ]);
 } catch (PDOException $e) {
     // Langsung hentikan skrip dan kirim error ke Railway Logs
